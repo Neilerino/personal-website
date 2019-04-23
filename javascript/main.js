67d3e0be9425c7a1afed9604302eb76032257bc0
Neil Wadden <neilwadden@pop-os.localdomain>
2019-04-22 23:14:48 -0230

    finished the about me page

--- javascript/main.js
+++ javascript/main.js
@@ -3,17 +3,24 @@ $(document).ready(() => {
 		$('#main-section').animate({
 			opacity: 0
 		}, 500, () => {
-			$('#main-section').toggleClass('d-none');
-			$('#about-section').toggleClass('d-none');
-			$('#about-section').animate({
-				opacity: 1
-			}, 500, () => {
-				$('#about-section').css('opacity', '1');
-			});
+			renderAboutMe();
 		});
 	});
 
 	$('#btn-exp').off().on('click', () => {
 
 	});
+
+	feather.replace();
 });
+
+
+function renderAboutMe() {
+	$('#main-section').toggleClass('d-none');
+	$('#about-section').toggleClass('d-none');
+	$('#about-section').animate({
+		opacity: 1
+	}, 500, () => {
+		$('#about-section').css('opacity', '1');
+	});
+}
